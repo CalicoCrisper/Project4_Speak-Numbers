a4c4e833606550fdf954fa099dc25e7743fee57c
Kira Ferrariiiiiiiiiiiiiiiiiiiiiii <ferrarakl@vcu.edu>
2025-10-17 21:57:26 -0400

    implement onSpeak(event)

--- script.js
+++ script.js
@@ -5,5 +5,21 @@ function generateRandomNumber () {
 return Math.floor(Math.random() * 100) + 1;
 }
 
+const randomNum = generateRandomNumber();
+
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+let recognition = new window.SpeechRecognition();
+
+// Start recognition and game
+recognition.start();
+
+//Capture user speech
+function onSpeak(event) {
+const msg = event.results[0][0].transcript;
+console.log(msg);
+}
 
 
+// Listen and handle speech event
+recognition.addEventListener('result', onSpeak);
